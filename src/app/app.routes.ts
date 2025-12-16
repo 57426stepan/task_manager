acbc8ec21eff1aaa98a4001b887ad7a07a2b7e64
s_svoboda <istepan33i@gmail.com>
2025-12-16 16:06:03 +0100

    Příprava struktury a Konfigurace routování

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'login', // Po startu jdi na login
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.routes').then(m => m.routes),
+    // Zde později přidáme ochranu (Guard), aby sem nemohl nepřihlášený
   },
 ];
