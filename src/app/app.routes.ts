b6322c47e6afc8c97acb866d72fe513b5cd74fb2
S_Svoboda <100199680+57426stepan@users.noreply.github.com>
2025-12-17 09:43:46 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,29 @@
 import { Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+// Funkce pro přesměrování
+// Pokud není přihlášen, pošli ho na login
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
+// Pokud UŽ JE přihlášen, pošli ho rovnou na úkoly (tabs)
+const redirectLoggedInToTabs = () => redirectLoggedInTo(['tabs']);
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login', // Po startu jdi na přihlášení
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
+    // Tady přidáme ochranu: Kdo je přihlášen, nesmí na login, ale jde rovnou do aplikace
+    ...canActivate(redirectLoggedInToTabs)
   },
   {
-    path: 'tabs', // Všechno, co začíná na "tabs", pošli do tabs.routes
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then(m => m.routes),
+    // Tady přidáme ochranu: Dovnitř smí jen přihlášený, jinak jde na login
+    ...canActivate(redirectUnauthorizedToLogin)
   },
 ];
